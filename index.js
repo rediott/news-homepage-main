319ebd96c8cc46f7a54c54bf16a15742c17ae6bd
Raihan Mufid Setiadi <82045772+rediott@users.noreply.github.com>
2023-05-11 13:53:01 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.height = "0";
+}
